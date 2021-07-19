a1ca0cf9b496f3d8286669809aa344da2e9e2b97
xiaohua2003 <xcai3@ualberta.ca>
2021-07-19 01:13:18 -0600

    update app.js

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ const app=Vue.createApp({
     //data, function
     data(){
         return{
+            url:'http://www.thenetninja.co.uk',
             showBook:"ture",
-            title:"Final Empire",
-            author:'Emily',
-            age:28
+            books:[
+                {title:'name of the wind', author: 'emily',img:'assets/1.jpg', isFav:true},
+                {title:'the way of kings', author: 'brandon',img:'assets/2.jpg', isFav:false},
+                {title:'the final empire', author: 'brandon',img:'assets/3.jpg', isFav:true}
+            ]
         }
     },
     methods:{
-        changeTitle(title){
-            this.title=title
+        toggleShowBooks(){
+            this.showBook= !this.showBook
         },
-        hidebook(){
-            this.showBook=false
+        handleEvent(e){
+            console.log(e,e.type)
+        },
+        handleMousemove(e){
+            this.x=e.offsetX
+            this.y=e.offsetY
 
         }
     }
